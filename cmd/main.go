4d98fae89784e1c2f2801643fa391240106878dd
Fajar <maulanafajaribrahim@gmail.com>
2023-12-05 17:11:41 +0700

    refactor(main): refactor by chat gpt suggestion

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,60 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"simple-blog-api-golang/internal/database"
 	"simple-blog-api-golang/internal/post"
 	"simple-blog-api-golang/pkg/router"
+	"syscall"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
 	}
 
 	db := database.InitDB()
-
-	err = db.AutoMigrate(&post.Post{})
-
-	if err != nil {
-		log.Fatal(err)
+	if err := db.AutoMigrate(&post.Post{}); err != nil {
+		log.Fatal("Error auto migrating database:", err)
 	}
 
-	fmt.Println("Server running on http://localhost:8080")
-	err = http.ListenAndServe(":8080", router.SetupRouter())
+	startServer()
+}
 
-	if err != nil {
-		log.Fatal(err)
+func startServer() {
+	fmt.Println("Server running on http://localhost:8080")
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router.SetupRouter(),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal("Error starting server:", err)
+		}
+	}()
+
+	waitForShutdown(&server)
+}
+
+func waitForShutdown(server *http.Server) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	fmt.Println("Shutting down the server...")
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Println("Error shutting down the server:", err)
 	}
 }
